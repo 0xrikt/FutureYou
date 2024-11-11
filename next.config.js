12a5532a35915ffb3c653f54cadc09c3ea0c8f29
0xrikt <taoyaqi1995@gmail.com>
2024-11-11 22:41:28 +0800

    fix:try next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-  }
-  
-  module.exports = nextConfig
+  reactStrictMode: true,
+  webpack(config) {
+    // 设置路径别名
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    config.resolve.alias['@/lib'] = path.resolve(__dirname, 'src/lib');
+    return config;
+  },
+}
+
+module.exports = nextConfig;
